d2f4d25d19880ad02de64c2b3371501dead11958
Sneha Menat <snehamenat029@gmail.com>
2024-08-28 20:04:13 +0530

    tested dataentry apis in backend

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,14 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 8088;
 
+
+
 // Middleware setup
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
 
 // Database connection
 connectToMongo(process.env.MONGO_URI || "mongodb+srv://tle:tle@tle.eryr0.mongodb.net/?retryWrites=true&w=majority&appName=TLE")
@@ -20,22 +24,29 @@ connectToMongo(process.env.MONGO_URI || "mongodb+srv://tle:tle@tle.eryr0.mongodb
     console.error("Error connecting to MongoDB Atlas:", err);
   });
 
-// Import and use the user routes
+
+//creating routes
 const userRoutes = require("./routes/user");
 const authorRoutes = require("./routes/author");
 const publicationRoutes = require("./routes/publications");
 const eventRoutes = require("./routes/event");
 const departmentRoutes = require("./routes/department");
 const collegeRoutes = require("./routes/colleges");
+const dataEntryRoutes = require("./routes/dataEntry");
 
 
 
+//routes
 app.use("/", userRoutes);
 app.use("/author", authorRoutes);
 app.use("/publications",  publicationRoutes);
 app.use("/events", eventRoutes);
 app.use("/departments", departmentRoutes);
 app.use("/colleges", collegeRoutes);
+app.use("/data-entries", dataEntryRoutes);
+
+
+
 
 // Root route for testing the server
 app.get("/", (req, res) => {
