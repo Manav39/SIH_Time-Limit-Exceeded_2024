4f081e674ad40ebd4a0d528fe8f093d6e50b258d
Mohammed Bhadsorawala <mhdbhadsorawala@gmail.com>
2024-09-04 00:08:15 +0530

    adding translations for admin sidebar

--- frontend/src/admin/SidebarAdmin.jsx
+++ frontend/src/admin/SidebarAdmin.jsx
@@ -1,47 +1,85 @@
-import React from "react";
-import {
-  FaHome,
-  FaUserGraduate,
-  FaUsers,
-  FaImage,
-  FaBriefcase,
-} from "react-icons/fa";
-import { ImBooks } from "react-icons/im";
-import { IoSettingsSharp, IoCalendar } from "react-icons/io5";
-import { RiSuitcaseFill } from "react-icons/ri";
-import { RiMoneyDollarBoxFill } from "react-icons/ri";
-import { MdForum } from "react-icons/md";
-import { Link, useLocation } from "react-router-dom";
+import React from 'react'
+import { FaHome, FaImage } from 'react-icons/fa'
+import { IoCalendar } from 'react-icons/io5'
+import { RiSuitcaseFill } from 'react-icons/ri'
+import { useTranslation } from 'react-i18next'
+import { Link, useLocation } from 'react-router-dom'
 
 const SidebarAdmin = ({ isOpen, toggleSidebar }) => {
-  const location = useLocation();
+  const location = useLocation()
+  const { t } = useTranslation()
 
   const isActive = (path) => {
-    return location.pathname === path ? "navactive" : "";
-  };
+    return location.pathname === path ? 'navactive' : ''
+  }
 
   return (
-    <aside id="sidebar" className={`sidebar ${isOpen ? "open" : ""}`}>
+    <aside id="sidebar" className={`sidebar ${isOpen ? 'open' : ''}`}>
       <ul className="sidebar-nav" id="sidebar-nav">
         <li onClick={toggleSidebar} className="nav-item">
           <Link
-            className={`nav-link ${isActive("/dashboard/deptcoord/depdash")}`}
+            className={`nav-link ${isActive('/dashboard/deptcoord/depdash')}`}
             to="/dashboard/deptcoord/depdash"
           >
             <FaHome />
-            <span className="ms-1">Admin</span>
+            <span className="ms-1">{t('adminSidebar.admin')}</span>
           </Link>
         </li>
         <li onClick={toggleSidebar} className="nav-item">
           <Link
-            className={`nav-link ${isActive("/dashboard/report")}`}
+            className={`nav-link ${isActive('/dashboard/report')}`}
             to="/dashboard/report"
           >
             <FaImage />
-            <span className="ms-1">Report</span>
+            <span className="ms-1">{t('adminSidebar.report')}</span>
           </Link>
         </li>
-        {/* <li onClick={toggleSidebar} className="nav-item">
+        <hr />
+        <li onClick={toggleSidebar} className="nav-item">
+          <Link
+            className={`nav-link ${isActive('/dashboard/events')}`}
+            to={'/dashboard/events'}
+          >
+            <IoCalendar />
+            <span className="ms-1">{t('adminSidebar.events')}</span>
+          </Link>
+        </li>
+        <li onClick={toggleSidebar} className="nav-item">
+          <Link
+            className={`nav-link ${isActive('/dashboard/adminpublications')}`}
+            to={'/dashboard/adminpublications'}
+          >
+            <RiSuitcaseFill />
+            <span className="ms-1">{t('adminSidebar.publications')}</span>
+          </Link>
+        </li>
+        <li onClick={toggleSidebar} className="nav-item">
+          <Link
+            className={`nav-link ${isActive('/dashboard/departments')}`}
+            to={'/dashboard/departments'}
+          >
+            <RiSuitcaseFill />
+            <span className="ms-1">{t('adminSidebar.departments')}</span>
+          </Link>
+        </li>
+        <li onClick={toggleSidebar} className="nav-item">
+          <Link
+            className={`nav-link ${isActive('/dashboard/users')}`}
+            to={'/dashboard/users'}
+          >
+            <RiSuitcaseFill />
+            <span className="ms-1">{t('adminSidebar.users')}</span>
+          </Link>
+        </li>
+      </ul>
+    </aside>
+  )
+}
+
+export default SidebarAdmin
+
+{
+  /* <li onClick={toggleSidebar} className="nav-item">
           <Link
             className={`nav-link ${isActive("/dashboard/courses")}`}
             to={"/dashboard/courses"}
@@ -49,36 +87,11 @@ const SidebarAdmin = ({ isOpen, toggleSidebar }) => {
             <ImBooks />
             <span className="ms-1">Courses</span>
           </Link>
-        </li> */}
-        <hr />
-        <li onClick={toggleSidebar} className="nav-item">
-          <Link
-            className={`nav-link ${isActive("/dashboard/events")}`}
-            to={"/dashboard/events"}
-          >
-            <IoCalendar />
-            <span className="ms-1">Events</span>
-          </Link>
-        </li>
-        <li onClick={toggleSidebar} className="nav-item">
-          <Link
-            className={`nav-link ${isActive("/dashboard/adminpublications")}`}
-            to={"/dashboard/adminpublications"}
-          >
-            <RiSuitcaseFill />
-            <span className="ms-1">Publications</span>
-          </Link>
-        </li>
-        <li onClick={toggleSidebar} className="nav-item">
-          <Link
-            className={`nav-link ${isActive("/dashboard/admindepartments")}`}
-            to={"/dashboard/admindepartments"}
-          >
-            <FaUserGraduate />
-            <span className="ms-1">Departments</span>
-          </Link>
-        </li>
-        {/* <li onClick={toggleSidebar} className="nav-item">
+        </li> */
+}
+
+{
+  /* <li onClick={toggleSidebar} className="nav-item">
           <Link
             className={`nav-link ${isActive("/dashboard/alumnilist")}`}
             to={"/dashboard/alumnilist"}
@@ -86,8 +99,10 @@ const SidebarAdmin = ({ isOpen, toggleSidebar }) => {
             <FaUserGraduate />
             <span className="ms-1">Alumni List</span>
           </Link>
-        </li> */}
-        {/* <li onClick={toggleSidebar} className="nav-item">
+        </li> */
+}
+{
+  /* <li onClick={toggleSidebar} className="nav-item">
           <Link
             className={`nav-link ${isActive("/dashboard/jobs")}`}
             to={"/dashboard/jobs"}
@@ -95,8 +110,10 @@ const SidebarAdmin = ({ isOpen, toggleSidebar }) => {
             <RiSuitcaseFill />
             <span className="ms-1">Jobs</span>
           </Link>
-        </li> */}
-        {/* <li onClick={toggleSidebar} className="nav-item">
+        </li> */
+}
+{
+  /* <li onClick={toggleSidebar} className="nav-item">
           <Link
             className={`nav-link ${isActive("/dashboard/donations")}`}
             to={"/dashboard/donations"}
@@ -104,8 +121,10 @@ const SidebarAdmin = ({ isOpen, toggleSidebar }) => {
             <RiMoneyDollarBoxFill />
             <span className="ms-1">Donations</span>
           </Link>
-        </li> */} 
-        {/* <li onClick={toggleSidebar} className="nav-item">
+        </li> */
+}
+{
+  /* <li onClick={toggleSidebar} className="nav-item">
           <Link
             className={`nav-link ${isActive("/dashboard/forum")}`}
             to={"/dashboard/forum"}
@@ -113,20 +132,5 @@ const SidebarAdmin = ({ isOpen, toggleSidebar }) => {
             <MdForum />
             <span className="ms-1">Forum</span>
           </Link>
-        </li> */}
-        <hr />
-        <li onClick={toggleSidebar} className="nav-item">
-          <Link
-            className={`nav-link ${isActive("/dashboard/users")}`}
-            to={"/dashboard/users"}
-          >
-            <FaUsers />
-            <span className="ms-1">Users</span>
-          </Link>
-        </li>
-      </ul>
-    </aside>
-  );
-};
-
-export default SidebarAdmin;
+        </li> */
+}
